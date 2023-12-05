24f3ceaaedd5b969d5dab9463606fcea321e76aa
Josimar Albert Castro Lopez <dev.jclopez@gmail.com>
2023-12-05 10:55:55 -0500

    Build application attemp 2

--- src/main/java/com/example/demo/student/StudentController.java
+++ src/main/java/com/example/demo/student/StudentController.java
@@ -1,11 +1,11 @@
 package com.example.demo.student;
 
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import javax.validation.Valid;
 import java.util.List;
 
 @RestController
